b6d8f6c5f1ae53b828d90ce41b15b099b92ce318
Nikola Shalamanov <nikolashalamanov999@gmail.com>
2025-03-17 07:26:03 +0200

    ServiceExtentionCollection added to clear out and simplify the Program.cs

---
+++ SharedTravelling/Extentions/ServiceCollectionExtention.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SharedTravelling.Data;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Extentions.DependencyInjection;
+
+public static class ServiceCollectionExtention
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		return services;
+	}
+
+	public static IServiceCollection AddApplicatonDbContext(this IServiceCollection services, IConfiguration config)
+	{
+		var connectionString = config.GetConnectionString("DefaultConnection");
+		services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(connectionString));
+
+		services.AddDatabaseDeveloperPageExceptionFilter();
+
+		return services;
+	}
+
+	public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+	{
+		services
+			.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+			.AddEntityFrameworkStores<ApplicationDbContext>();
+
+		return services;
+	}
+}
