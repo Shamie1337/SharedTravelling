cbb25404e661fab52cfe4b1f4d07a9fa356532c7
Nikola Shalamanov <nikolashalamanov999@gmail.com>
2025-03-17 10:13:33 +0200

    MainMenu component added and cleared

---
+++ SharedTravelling/Components/MainMenuComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace SharedTravelling.Components
+{
+	public class MainMenuComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return await Task.FromResult<IViewComponentResult>(View());
+		}
+	}
+}
